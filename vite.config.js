be4068a372af7c4aa5560384947fabd971cad368
Vitaliy <124338899+opa33@users.noreply.github.com>
2025-07-05 00:06:56 +0500

    feat: readme файл

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    server: {
+        host: true,
+        port: 3000,
+        allowedHosts: ["bafe-2a0d-8480-1-222a-00.ngrok-free.app"],
+    },
+});
